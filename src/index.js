781ce76ff4c4086ab5de66e621cf0cf23d049730
Vivian Gomez <viviangomez0@gmail.com>
2021-11-20 17:25:23 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//document.addEventListener("DOMContentLoaded", () => {
+  //document.getElementById('create-task-form').innerHTML +="Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+  //event.preventDefault();
+//});
+document.querySelector("#create-task-form").addEventListener("click", function(event) {
+  document.getElementById("list").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+  event.preventDefault();
+}, false);
+
